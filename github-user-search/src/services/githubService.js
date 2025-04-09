ccd8368aca55ae7b3b1581f14869603dc5990c54
Taofeek Ayuba <taofeekayubaabiodun@gmail.com>
2025-04-09 20:30:26 +0100

    second step

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,18 +1,12 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/Toafeek-Abbey';
+const fetchUserData = () => {
+    axios.get("https://github.com/Toafeek-Abbey")
+    .then(res => {
+        return res.data
+    }).catch(err => {
+        console.log(err)
+    })
+}
 
-/**
- * Fetch user data from GitHub by username
- * @param {string} username - GitHub username to search
- * @returns {Promise<Object>} - GitHub user data
- */
-export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`${BASE_URL}/${username}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching GitHub user data:', error);
-    throw error;
-  }
-};
+export default fetchUserData
