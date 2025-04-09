6d8e9acfd25f0212d0a04f1ee9261531056a0ddc
Taofeek Ayuba <taofeekayubaabiodun@gmail.com>
2025-04-09 14:29:25 +0100

    add sm

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,7 +10,7 @@ const Homepage = () => {
     }, []);
   
     return (
-      <div className="p-6 bg-gray-100 min-h-screen" >
+      <div className="sm:p-4 p-6 bg-gray-100 min-h-screen" >
         <h1 className="text-3xl font-bold mb-6 text-center hover:text-blue-500">🍽️ Recipe List</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 hover:shadow-xl">
           {recipes.map(recipe => (
