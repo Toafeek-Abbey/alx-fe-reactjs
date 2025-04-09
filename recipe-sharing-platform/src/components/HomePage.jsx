b5756f682e86ce75c55ee1931f85af72eae10162
Taofeek Ayuba <taofeekayubaabiodun@gmail.com>
2025-04-09 14:24:44 +0100

    add hover

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -11,10 +11,10 @@ const Homepage = () => {
   
     return (
       <div className="p-6 bg-gray-100 min-h-screen" >
-        <h1 className="text-3xl font-bold mb-6 text-center">🍽️ Recipe List</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <h1 className="text-3xl font-bold mb-6 text-center hover:text-blue-500">🍽️ Recipe List</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 hover:shadow-xl">
           {recipes.map(recipe => (
-            <div key={recipe.id} className="bg-white shadow-xl rounded-2xl overflow-hidden">
+            <div key={recipe.id} className="bg-white shadow-xl rounded-2xl overflow-hidden hover:shadow-xl">
               <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover" />
               <div className="p-4">
                 <h2 className="text-xl font-semibold">{recipe.title}</h2>
