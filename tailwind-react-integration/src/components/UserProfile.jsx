14c81b937a0fc3d703bed3abd0c2a7bb5395eccf
Taofeek Ayuba <taofeekayubaabiodun@gmail.com>
2025-04-09 13:04:07 +0100

    second correction

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-8 md:p-8 sm:p-4 max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
-        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36-md h-36-md w-24-sm h-24-sm mx-auto"/>
+      <div className="bg-gray-100 p-8 md:p-8 sm:p-4 md:max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
+        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto"/>
         <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
         <p className="sm:text-sm sm:text-base text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
