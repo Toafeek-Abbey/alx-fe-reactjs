93d2e38908890ddd5f9952f5f7fa68c6ebe72c95
Taofeek Ayuba <taofeekayubaabiodun@gmail.com>
2025-04-09 21:23:08 +0100

    error handling

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
-
+let filters = {}
 const fetchUserData = () => {
+    const { username, location, repos } = filters
+
+    // Start with the main username (required for search)
+    let query = username ? `${username}` : '';
+  
+    // Add advanced filters
+    if (location) query += `+location:${location}`;
+    if (repos) query += `+repos:>${repos}`; // can adjust operator to >=, =, etc.
+
     axios.get("https://api.github.com")
     .then(res => {
         return res.data
