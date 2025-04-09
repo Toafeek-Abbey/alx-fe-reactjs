b46d49891f1bbcfb15d3c8b0012214b27b9d3978
Taofeek Ayuba <taofeekayubaabiodun@gmail.com>
2025-04-09 21:48:42 +0100

    update github service

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 let filters = {}
 const fetchUserData = () => {
-    const { username, location, repos } = filters
+    const { username, location, minRepos } = filters
 
     // Start with the main username (required for search)
     let query = username ? `${username}` : '';
   
     // Add advanced filters
     if (location) query += `+location:${location}`;
-    if (repos) query += `+repos:>${repos}`; // can adjust operator to >=, =, etc.
-
-    axios.get("https://api.github.com")
+    if (minRepos) query += `+repos:>${minRepos}`; // can adjust operator to >=, =, etc.
+    axios.get("https://api.github.com/search/users?q")
     .then(res => {
         return res.data
     }).catch(err => {
