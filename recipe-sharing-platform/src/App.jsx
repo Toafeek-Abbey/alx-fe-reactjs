4f935b772ab506dd86491a1a6349a761e254c393
Taofeek Ayuba <taofeekayubaabiodun@gmail.com>
2025-04-09 14:05:56 +0100

    Other commit

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Homepage from './components/HomePage'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+      < Homepage />
+      
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,9 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        
         <p className='text-blue-500'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
